969231ede4899e5b0b5bd67dc694e60d8f44c4de
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-01 20:06:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from models import db, User
+from forms import RegisterForm, LoginForm
